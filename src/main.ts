7366cd3f32150d00c7e61907990453dc84d7181b
fine <dlgmltjr0925@gmail.com>
2021-11-01 14:24:28 +0900

    feature: Swagger Bearer Security 적용

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     .setTitle('Board Rank')
     .setDescription('API for board rank')
     .setVersion('1.0')
+    .addSecurity('bearer', { type: 'http', scheme: 'Bearer' })
     .addTag('games')
     .build();
 

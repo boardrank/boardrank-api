a26f95784321121e59ee464547c4c2a327caf8cc
fine <dlgmltjr0925@gmail.com>
2021-12-10 17:42:44 +0900

    feature: develop branch trigger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/health')
+  @Get('/healthy')
   health(): string {
-    return this.appService.health();
+    return this.appService.healthy();
   }
 }
